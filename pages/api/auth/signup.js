327bec70771a8c923532da262f4ffde1fb556592
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-19 12:21:53 +0200

    feat(UX): Add a User Signup API Route

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,23 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  const data = req.body;
+  const { email, password } = data;
+
+  if (!email || !email.includes("@") || !password || password.trim().length < 7) {
+    res.status(422).json({ message: "Invalid input - password should also be at least 7 characters long." });
+    return;
+  }
+
+  const client = await connectToDatabase();
+  const db = client.db();
+
+  const result = await db.collection("users").insertOne({
+    email,
+    password: await hashPassword(password),
+  });
+
+  res.status(201).json({ message: "Created user!" });
+}
+export default handler;
