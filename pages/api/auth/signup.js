7746267707efef4b55e55927bb84783ebf5a7693
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-25 12:21:15 +0200

    feat(UX): Improve Signup w/unique Email address

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -9,7 +9,10 @@ async function handler(req, res) {
   const data = req.body;
   const { email, password } = data;
 
-  if (!email || !email.includes("@") || !password || password.trim().length < 7) {
+  if (!email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7) {
     res.status(422).json({ message: "Invalid input - password should also be at least 7 characters long." });
     return;
   }
@@ -17,6 +20,15 @@ async function handler(req, res) {
   const client = await connectToDatabase();
   // * INFO: Name of the DB
   const db = client.db('auth-demo');
+
+  const existingUser = await db
+    .collection("users")
+    .findOne({ email });
+  if (existingUser) {
+    res.status(422).json({ message: "User exists already!" });
+    client.close();
+    return;
+  }
 
   const result = await db.collection("users").insertOne({
     email,
