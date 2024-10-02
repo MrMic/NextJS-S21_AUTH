6f196396968876acfeb9076f0055a76eba86d699
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-02 12:34:44 +0200

    feat(UI/UX): Manage Active Session (on the Frontend)

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 
 import classes from './main-navigation.module.css';
+import { useSession } from 'next-auth/react';
 
 function MainNavigation() {
+  const { data: session, status } = useSession();
+
+
   return (
     <header className={classes.header}>
       <Link href="/">
@@ -10,15 +14,21 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !status && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
