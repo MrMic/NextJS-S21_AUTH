538127e4ecf53d0fe12502dc351cc19eb0fc54c1
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-16 10:46:07 +0200

    feat(UX): Add "change password" logic

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from "next-auth/react";
+import { connectToDatabase } from "../../../lib/db";
+import { hashPassword, verifyPassword } from "../../../lib/auth";
 
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
@@ -11,6 +13,39 @@ async function handler(req, res) {
     res.status(401).json({ message: 'Not authenticated!' });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+  const usersCollection = client.db('auth-demo').collection('users');
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found.' });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(403).json({ message: 'Invalid password. Try again.' });
+    client.close();
+    return;
+  }
+
+  const result = usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: await hashPassword(newPassword) } }
+  )
+
+  res.status(200).json({ message: 'Password updated!' });
+  client.close();
 }
 
 export default handler
